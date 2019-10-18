22d8c4112161f9efa609016fbb48b9e8aa18e1f7
Byungchan Gil <gilbc890@gmail.com>
2019-10-18 18:28:43 -0500

    save weather data in firebase

--- App.js
+++ App.js
@@ -8,17 +8,22 @@ import { database } from './firebase';
 
 export default function App() {
   const [firstCondition, setFirstCondition] = useState();
-  const [secondCondition, setSecondCondition] = useState();
-  const [thirdCondition, setThirdCondition] = useState();
-  const [fourthCondition, setFourthCondition] = useState();
   const [firstTemp, setFirstTemp] = useState();
-  const [secondTemp, setSecondTemp] = useState();
-  const [thirdTemp, setThirdTemp] = useState();
-  const [fourthTemp, setFourthTemp] = useState();
   const [firstDt_txt, setFirstDt_txt] = useState();
+
+  const [secondCondition, setSecondCondition] = useState();
+  const [secondTemp, setSecondTemp] = useState();
   const [secondDt_txt, setSecondDt_txt] = useState();
+
+  const [thirdCondition, setThirdCondition] = useState();
+  const [thirdTemp, setThirdTemp] = useState();
   const [thirdDt_txt, setThirdDt_txt] = useState();
+
+  const [fourthCondition, setFourthCondition] = useState();
+  const [fourthTemp, setFourthTemp] = useState();
   const [fourthDt_txt, setFourthDt_txt] = useState();
+
+  const [Loaded, setLoaded] = useState(false);
 
   const getWeatherForecast = async (city, country) => {
     const { data:{
@@ -42,6 +47,85 @@ export default function App() {
     setFourthCondition(list[6].weather[0].main)
     setFourthTemp(list[6].main.temp)
     setFourthDt_txt(list[6].dt_txt)
+
+    setLoaded(true);
+
+    if(Loaded){
+      saveWeatherData(
+        firstCondition,
+        firstTemp,
+        firstDt_txt,
+  
+        secondCondition,
+        secondTemp,
+        secondDt_txt,
+  
+        thirdCondition,
+        thirdTemp,
+        thirdDt_txt,
+  
+        fourthCondition,
+        fourthTemp,
+        fourthDt_txt,
+  
+        city
+      )  
+    }
+  }
+
+  const saveDataCity = (city, country) => {
+    const db = database.ref(`/test`);
+    db.update({
+      cities: {city}
+    });
+    saveDataCountry(city, country)
+  }
+  const saveDataCountry = (city, country) => {
+    const db = database.ref(`/test/cities/${city}`);
+    db.update({
+      country: country
+    });
+  }
+
+  const saveWeatherData = (
+    firstCondition,
+    firstTemp,
+    firstDt_txt,
+
+    secondCondition,
+    secondTemp,
+    secondDt_txt,
+
+    thirdCondition,
+    thirdTemp,
+    thirdDt_txt,
+
+    fourthCondition,
+    fourthTemp,
+    fourthDt_txt,
+
+    city
+  ) => {
+    const db = database.ref(`/test/cities/${city}`);
+    db.update({
+      weather: {    
+        firstCondition,
+        firstTemp,
+        firstDt_txt,
+
+        secondCondition,
+        secondTemp,
+        secondDt_txt,
+
+        thirdCondition,
+        thirdTemp,
+        thirdDt_txt,
+
+        fourthCondition,
+        fourthTemp,
+        fourthDt_txt
+    }
+    });  
   }
   
   const getLocation = async () => {
@@ -49,22 +133,27 @@ export default function App() {
       await Location.requestPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
       const city = await Location.reverseGeocodeAsync({latitude, longitude});
-      getWeatherForecast(city[0].city, city[0].isoCountryCode);
+
+      if(checkCity(city[0].city)){
+        getWeatherForecast(city[0].city, city[0].isoCountryCode);
+        saveDataCity(city[0].city, city[0].isoCountryCode);  
+      } else{
+        console.log('same city')
+      }
     } catch (error) {
       Alert.alert("Please allow us to get your place");
     }
   };  
 
-  //test
-  const getfirebase = () => {
-    const test = database.ref();
-    test.on('value', (snapshot) => {
-      console.log(snapshot.val())
+  const checkCity = (currentCity) => {
+    const cities = database.ref(`/test/cities`);
+    cities.on('value', (snapshot) => {
+      const cityName = Object.entries(snapshot.val())[0][0];
+      return cityName !== currentCity
     });
   }
-  
 
-  useEffect(() => {
+  useEffect (() => {
     getLocation();
   });
 
